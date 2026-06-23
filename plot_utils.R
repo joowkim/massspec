c81da2c59c68ffb9f70786cc4f1684d87f209d00
joowkim <joowookbio@gmail.com>
2026-06-23 11:08:01 -0400

    Update static md func

--- plot_utils.R
+++ plot_utils.R
@@ -237,8 +237,8 @@ static_md_plot <- function(dat_df, hline_y) {
   plt <-  ggplot(dat_df) + geom_point(aes(x = AveExpr, y=logFC, color=Sig)) +
     geom_hline(yintercept = hline_y, color = 'red') +
     scale_color_manual(values = c("black", "salmon")) +
-    ylim(-4, 4) +
-    xlim(4, 25) +
+    ylim(-3, 3) +
+    xlim(0, 20) +
     xlab("average log expression")
 
   return (plt)
